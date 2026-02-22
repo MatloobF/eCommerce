637ebd833939d78b7a52b6c0670a733bc711b873
MatloobF <farooquimatloob9@gmail.com>
2026-02-22 17:28:06 +0530

    additional Hooks

--- frontend/src/Signup/index.jsx
+++ frontend/src/Signup/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './style.scss' ;
 import { Card, CardBody, CardFooter, CardHeader, FormGroup, FormLabel, FormControl, Container, Col, Row} from 'react-bootstrap';
+import {ACTIONS, initialState, signupReducer } from './signupReducer.js';
 
 const Signup = () => {
+  const [state, dispatch] = useReducer(signupReducer, initialState);
+
+  const isPassworldValid = [...Object.values(state.password.validations)].every(Boolean);
+
   return (
-    
     <Container fluid>
         <Row>
             <Col sm={{span: 10, offset: 1}} md= {{span: 6, offset: 3}} lg= {{span: 4, offset: 4}}>
@@ -13,22 +17,22 @@ const Signup = () => {
                     <CardBody>
                         <FormGroup controlId= 'name' className='mb-3'>
                             <FormLabel>Name</FormLabel>
-                            <FormControl placeholder="Enter name" /> 
+                            <FormControl onChange={(e)=>dispatch({type: ACTIONS.NAME_CHANGE, payload: e.target.value})} placeholder="Enter name" /> 
                         </FormGroup>
 
                         <FormGroup controlId= 'username' className='mb-3'>
                             <FormLabel>Username</FormLabel>
-                            <FormControl placeholder="Enter username" /> 
+                            <FormControl onChange={(e)=>dispatch({type: ACTIONS.USERNAME_CHANGE, payload: e.target.value})} placeholder="Enter username" /> 
                         </FormGroup>
 
                         <FormGroup controlId= 'email' className='mb-3'>
                             <FormLabel>Email</FormLabel>
-                            <FormControl type="email" placeholder="Enter email" /> 
+                            <FormControl onChange={(e)=>dispatch({type: ACTIONS.EMAIL_CHANGE, payload: e.target.value})} type="email" placeholder="Enter email" /> 
                         </FormGroup>
 
                         <FormGroup controlId= 'password' className='mb-3'>
                             <FormLabel>Password</FormLabel>
-                            <FormControl type="password" placeholder="Enter password" /> 
+                            <FormControl onChange={(e)=>dispatch({type: ACTIONS.PASSWORD_CHANGE, payload: e.target.value})} type="password" placeholder="Enter password" /> 
                         </FormGroup>
 
      
